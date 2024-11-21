cb0cceb0336b07a09224eecddbd2cd21fe078fae
Raz <AT STUDENT POINT FT>
2024-11-21 19:08:58 +0100

    wwww

--- Makefile
+++
@@ -1,6 +0,0 @@
-CFLAGS = -Wall -Wextra -Werror -g
-
-filter:
-
-test: filter
-	valgrind ./filter fuck42
--- filter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-[ -z "$1" ]
-x=$(echo "$1" | sed 's/\([][(){}\^.*+?$|]\)/\\\1/g')
-y=$(echo "$1" | sed -E 's/./*/g')
-echo x=$x;
-echo y=$y;
-sed -E "s/$x/$y/"
