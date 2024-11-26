fe3190dcb138ce95faa5f0c6daefd60116d2aff4
Raphael Zaccaria <rzaccari@student.42lausanne.ch>
2024-11-26 14:29:53 +0100

    I suppose this should pass the exam

--- filter/filter.c
+++ filter/filter.c
@@ -6,7 +6,7 @@
 /*   By: razaccar <razaccar@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 18:23:11 by razaccar          #+#    #+#             */
-/*   Updated: 2024/11/22 18:23:32 by razaccar         ###   ########.fr       */
+/*   Updated: 2024/11/26 13:47:30 by razaccar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 #include <string.h>
 
 #define BUFFER_SIZE 1
-
 
 size_t ft_strlen(char *str)
 {	
@@ -40,6 +39,8 @@ char	*ft_strjoin(char *s1, char *s2)
 	s1len = ft_strlen(s1);
 	s2len = ft_strlen(s2);
 	s1 = realloc(s1, s1len + s2len  + 1);
+	if (!s1)
+		perror("Error:");
 	s1[s1len + s2len] = 0;
 	while (*s2)
 		s1[s1len + (i++)] = *(s2++);
@@ -50,22 +51,22 @@ char	*get_input(char *line, char *buffer, int *stop)
 {
 	size_t	read_len;
 
-	while (1)
+	read_len = read(0, buffer, BUFFER_SIZE);
+	if (read_len < 0)
+		perror("Error:");
+	while (read_len > 0)
 	{
+		buffer[read_len] = 0;
+		line = ft_strjoin(line, buffer);
 		if (line && strchr(line, '\n'))
 		{
 			*(strchr(line, '\n') + 1) = 0;
 			break ;
 		}
 		read_len = read(0, buffer, BUFFER_SIZE);
-		if (read_len == 0)
-		{
-			*stop = 1;
-			break ;
-		}
-		buffer[read_len] = 0;
-		line = ft_strjoin(line, buffer);
 	}
+	if (read_len == 0)
+		*stop = 1;
 	return (line);
 }
 
@@ -96,13 +97,13 @@ void	print_filtered(char *line, char *arg)
 int	main(int argc, char **argv)
 {
 	char	buffer[BUFFER_SIZE + 1];
-	int	stop;
 	char	*line;
+	int		stop;
 
 	if (argc == 1)
 		argv[1] = "";
-	stop = 0;
 	line = NULL;
+	stop = 0;
 	while (1)
 	{
 		line = get_input(line, buffer, &stop);
@@ -115,4 +116,5 @@ int	main(int argc, char **argv)
 		}
 		break ;
 	}
+	return (0);
 }
